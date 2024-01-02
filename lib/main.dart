125b62e1bd05c43047e8d0faa046c33bfca0bc72
obehi234 <obehi.ataga234@gmail.com>
2024-01-02 16:47:24 +0100

    Set up audio player

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const XylophoneApp());
 }
 
 class XylophoneApp extends StatelessWidget {
-  const XylophoneApp({super.key});
+  const XylophoneApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
-        )
-      )
+          child: Center(
+            child: TextButton(
+              onPressed: () async {
+                final player = AudioPlayer();
+                await player.setSource(AssetSource('note1.wav'));
+                print("Sound played");
+              },
+              child: const Text('Click Me'),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
-
-
-
-
-
